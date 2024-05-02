959ef6f2a07d6d6aa71243b16922098c543adc75
Tower-Babel <123087201+Tower-Babel@users.noreply.github.com>
2024-05-02 09:53:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 st.write("#")
+st.write("#")
+st.write("#")
 st.markdown("<h1 style='color: #e87723; font-family: High Tower Text;'>Eli Lily Consolidated Revenue</h1>", unsafe_allow_html=True)
 
 
